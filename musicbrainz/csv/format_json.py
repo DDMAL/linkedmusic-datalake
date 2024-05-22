470e9f0de9e2d2d31ed7c95955e31d235065f0fc
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-05-22 13:03:50 -0400

    test case 2 added

--- musicbrainz/csv/format_json.py
+++ musicbrainz/csv/format_json.py
@@ -4,7 +4,7 @@ def format() -> None:
     """
     Format the downloaded json file, which is not readable for python json library.
     """
-    with open("test", 'r') as json_file:
+    with open("test2", 'r') as json_file:
         with open("json_init.json", 'w') as out:
             out.write("[")
             lines = json_file.readlines()
