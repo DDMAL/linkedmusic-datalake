33aa12fc10e1af58f74df04aee9471b8c633ce6c
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-05-22 09:02:03 -0400

    Update convert_to_csv.py

--- musicbrainz/csv/convert_to_csv.py
+++ musicbrainz/csv/convert_to_csv.py
@@ -5,5 +5,18 @@ with open("json_init.json", 'r') as json_file:
     json_data = json.load(json_file)
     # print(json_data)
     
+    header = []
+    cnt = 0
+    values = []
     for element in json_data:
-        print()
+        for k, v in element:
+            value_of_line = {}
+            if (not (isinstance(v, dict) and isinstance(v, list))):
+                if k not in header:
+                    header += k
+                value_of_line[k] = v
+            values += value_of_line
+            
+            
+            
+            
