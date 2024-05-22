78f5abdac8f97bc8132c8d623394962c27f9bd57
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-05-22 10:28:09 -0400

    Update convert_to_csv.py

--- musicbrainz/csv/convert_to_csv.py
+++ musicbrainz/csv/convert_to_csv.py
@@ -1,13 +1,13 @@
 import json
 import csv
-
+header = []
+cnt = 0
+values = []
+    
 with open("json_init.json", 'r') as json_file:
     json_data = json.load(json_file)
     # print(json_data)
     
-    header = []
-    cnt = 0
-    values = []
     for element in json_data:
         for k, v in element:
             value_of_line = {}
@@ -17,6 +17,25 @@ with open("json_init.json", 'r') as json_file:
                 value_of_line[k] = v
             values += value_of_line
             
+    
+with open("out.csv", "w") as out:
+    # write header
+    line = "id,"
+    for column in header:
+        if column != "id":
+            line += column
+        
+        if column != header[-1]:
+            line += ','
+    out.writelines()
             
+    line = ""
+    for row in values:
+        for column in header:
+            line += row[column]
             
+            if column != header[-1]:
+                line += ','
+    out.writelines()
+        
             
