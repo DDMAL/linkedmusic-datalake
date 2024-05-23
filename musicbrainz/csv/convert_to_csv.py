9f35c672b0a3073667393979040b9413deb06668
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-05-23 12:49:56 -0400

    Update convert_to_csv.py
    Trying the recursive path finding algorithm to interpret nested lists and dictionaries in the musicbrainz file. Does not work entirely now, but it's getting there.

--- musicbrainz/csv/convert_to_csv.py
+++ musicbrainz/csv/convert_to_csv.py
@@ -2,68 +2,86 @@ import json
 import format_json
 
 header = ["id"]
-cnt = 0
 values = []
 
 format_json.format()
-    
+
 with open("json_init.json", 'r') as json_file:
     json_data = json.load(json_file)
     # print(json_data)
-    
-for element in json_data:
-    value = {}
-    for k in element:
-        v = element[k]
-        
-        if (isinstance(v, str)):
-            if k not in header:
-                header.append(k)
-            
-            temp = str(v).replace("\r\n", "")
-            temp = temp.replace(",", "`")
-            value[k] = temp
-            
-        elif (isinstance(v, dict)):
-            if "id" in v:
-                if k + "_id" not in header:
-                    header.append(k + "_id")
-                value[k + "_id"] = v["id"]
-            
-            if "name" in v:
-                if k + "_name" not in header:
-                    header.append(k + "_name")
-                value[k + "_name"] = v["name"]
-                            
-    values.append(value)
-                
-# print(header)
-# print(values)
 
-with open("out.csv", "w") as out:
-    # write header
-    line = "id,"
-    for column in header:
-        if column == "id":
-            continue
+def extract(data, first_level : bool=True, key : str="", value : dict={}):
+    if key != "":
+        first_level = False
+                    
+    if (isinstance(data, dict)):
+        if first_level:
+            global values
+            first_level = False
+            for k in data:
+                extract(data[k], first_level, k, value)
+            first_level = True
+            values.append(value)
+            value = {}
+
+        else:
+            for k in data:
+                if k == "id":
+                    extract(data["id"], first_level, key + "_id", value)
+                
+                if k == "name":
+                    extract(data["name"], first_level, key + "_name", value)
+                    
+                if (isinstance(data[k], dict) or isinstance(data[k], list)):
+                    extract(data[k], first_level, key + "_" + k, value)
+
+                
+    elif (isinstance(data, list)):
+        rep_count = 0
+        for element in data:
+            rep_count += 1
+            if (isinstance(element, dict)):
+                extract(element, first_level, key + str(rep_count), value)
+                
+    else:
+        global header
+        if key not in header:
+            header.append(key)
             
-        line += column
-        
-        if column != header[-1]:
-            line += ','
-    out.writelines(line)
-    out.writelines("\n")
-            
-    line = ""
-    for row in values:
+        value[key] = data
+        return
+                                
+
+if __name__ == "__main__":
+    extract(json_data)
+    # print(header)
+    # print(values)
+    with open("out.csv", "w") as out:
+        # write header
+        line = "id,"
         for column in header:
-            if column in row:
-                line += str(row[column])
+            if column == "id":
+                continue
+                
+            line += column
             
             if column != header[-1]:
                 line += ','
-            else:
-                line += "\n"
-    out.writelines(line)
-        
+                
+        out.writelines(line)
+        out.writelines("\n")
+                
+        line = ""
+        for row in values:
+            for column in header:
+                if column in row:
+                    line += str(row[column])
+                
+                if column != header[-1]:
+                    line += ','
+                else:
+                    line += "\n"
+                    
+        out.writelines(line)
             
+                
