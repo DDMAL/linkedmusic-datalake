4dff0b161cc23129f4c4c43a9e53f2cc9fc476be
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-05-24 10:53:39 -0400

    Updated convert_to_csv.py
    The code now works with recursive search of nested lists and dictionaries (up to 3 times).
    Also deleted all unnecessary files and updated .gitignore according to comments.

--- musicbrainz/csv/convert_to_csv.py
+++ musicbrainz/csv/convert_to_csv.py
@@ -1,92 +1,97 @@
+# This file converts a JSON Lines file from MusicBrainz to a CSV file
+# For OpenRefine Reconciling
+# Input file can be downloaded in 
+# https://data.metabrainz.org/pub/musicbrainz/data/json-dumps/20240522-001002/
 import json
-import format_json
+import copy
 
 header = ["id"]
 values = []
 
-format_json.format()
+with open("test", "r") as f:
+    json_data = [json.loads(m) for m in f]
 
-with open("json_init.json", 'r') as json_file:
-    json_data = json.load(json_file)
-    # print(json_data)
+# print(json_data)
 
-def extract(data, first_level : bool=True, key : str="", value : dict={}):
+def extract(data, first_level: bool = True, key: str = "", value: dict = {}):
     if key != "":
         first_level = False
-                    
-    if (isinstance(data, dict)):
+
+    if isinstance(data, dict):
         if first_level:
             global values
-            first_level = False
             for k in data:
-                extract(data[k], first_level, k, value)
-            first_level = True
-            values.append(value)
+                extract(data[k], False, k, value)
+            values.append(copy.deepcopy(value))
             value = {}
 
         else:
             for k in data:
                 if k == "id":
                     extract(data["id"], first_level, key + "_id", value)
-                
+
                 if k == "name":
                     extract(data["name"], first_level, key + "_name", value)
-                    
-                if (isinstance(data[k], dict) or isinstance(data[k], list)):
+
+                if isinstance(data[k], dict) or isinstance(data[k], list):
                     extract(data[k], first_level, key + "_" + k, value)
 
-                
-    elif (isinstance(data, list)):
+    elif isinstance(data, list):
         rep_count = 0
         for element in data:
             rep_count += 1
-            if (isinstance(element, dict)):
+            if isinstance(element, dict) and rep_count <= 3:
                 if first_level:
                     rep_count = 0
                     extract(element, first_level, key, value)
                 else:
                     extract(element, first_level, key + str(rep_count), value)
 
-                
     else:
         global header
         if key not in header:
             header.append(key)
+
+        if isinstance(data, str):
+            data = data.replace(",", "")
+            data = data.replace("\r\n", "")
+        
+        if data == None:
+            data = ""
             
         value[key] = data
         return
-                                
+
 
 if __name__ == "__main__":
     extract(json_data)
-    print(header[0:90])
+    # print(header[0:90])
     # print(values)
     with open("out.csv", "w") as out:
         # write header
         line = "id,"
+        header.sort(key=len)
         for column in header:
             if column == "id":
                 continue
-                
+
             line += column
-            
+
             if column != header[-1]:
-                line += ','
-                
+                line += ","
+
         out.writelines(line)
         out.writelines("\n")
-                
+
         line = ""
         for row in values:
             for column in header:
                 if column in row:
                     line += str(row[column])
-                
+
                 if column != header[-1]:
-                    line += ','
+                    line += ","
                 else:
                     line += "\n"
-                    
+
         out.writelines(line)
-            
-                
