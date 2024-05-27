1209cdf6bff4abcdc8b8bf6c04c70c23fa06b104
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-05-27 12:46:36 -0400

    Update convert_to_csv.py

--- musicbrainz/csv/convert_to_csv.py
+++ musicbrainz/csv/convert_to_csv.py
@@ -27,54 +27,74 @@ def extract(data, value: dict, first_level: bool = True, key: str = ""):
     '''
     if key != "":
         first_level = False
+        
     if "relations" in key or "aliases" in key or "tags" in key:
         # ignore relations and aliases to make output simplier
         return
 
     if isinstance(data, dict):
+        # the input JSON Lines format is lines of dictionaries, and the input data should be
+        # a list of dictionaries.
         if first_level:
+            # if this dictionary is not nested i.e. first level, then we extract every entry.
             global values
+            # make a new entry for the value list, since each first level dictionary
+            # in JSON file represents a new instance. This value dictionary is carried and 
+            # preserved between each recursive call.
             value = {}
             for k in data:
                 extract(data[k], value, False, k)
+            
+            # after extracting every entry of the current line, append it to the list and empty it.
             values.append(copy.deepcopy(value))
             value = {}
 
         else:
+            # if this dictionary is nested, then we do not extract all info,
+            # we only need the id and the name of that dictionary.
             for k in data:
                 if k == "id":
+                    # extract its id
                     extract(data["id"], value, first_level, key + "_ids")
 
                 if k == "name":
+                    # extract its name
                     extract(data["name"], value, first_level, key + "_name")
 
                 if isinstance(data[k], dict) or isinstance(data[k], list):
+                    # if there is still a nested instance, extract further
                     extract(data[k], value, first_level, key + "_" + k)
 
     elif isinstance(data, list):
         rep_count = 0
+        # extract each element of the list.
         for element in data:
             rep_count += 1
             if isinstance(element, dict) and rep_count <= 3:
                 if first_level:
+                    # if it's first level, we reset the counter.
                     rep_count = 0
                     extract(element, value, first_level, key)
                 else:
+                    # we add the counter to the key string if it's not first level.
+                    # up to 3 elements from each list is extracted.
                     extract(element, value, first_level, key + str(rep_count))
 
     else:
+        # if data is not a collection, we parse it and add to the current value dictionary.
         global header
         if key not in header:
             header.append(key)
 
+        v = data
         if isinstance(data, str):
-            data = data.replace(",", "_")
-            data = data.replace("\r\n", "")
+            v = v.replace(",", "_")
+            v = v.replace("\r\n", "")
 
         if data is None:
-            data = ""
+            v = ""
 
-        value[key] = data
+        value[key] = v
         return
 
 
@@ -86,7 +106,7 @@ if __name__ == "__main__":
         writer = csv.writer(out)
         
         header.sort(key=len)
-        # id must be the first column of the csv file.
+        # id must be the first column of the csv file. Reorder the header.
         header.remove("id")
         header.insert(0, "id")
     
