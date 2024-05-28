31964620f798a64c4e42979530d67f8685569f5a
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-05-28 13:00:28 -0400

    Update script and file paths

--- musicbrainz/csv/reconcile.py
+++ musicbrainz/csv/reconcile.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import requests
+import os
+import re
 
 # Load the CSV file
-file_path = 'out.csv'
-data = pd.read_csv(file_path)
+DIRNAME = os.path.dirname(__file__)
+FILENAME = os.path.join(DIRNAME, 'data/out.csv')
+data = pd.read_csv(FILENAME)
+OUTNAME = os.path.join(DIRNAME, 'data/reconciled.csv')
+OUTDATA = pd.DataFrame()
 
 # Function to reconcile IDs with MusicBrainz
 def reconcile_musicbrainz(id, entity_type='recording'):
@@ -31,10 +36,26 @@ def reconcile_wikidata(query, type='item'):
             return results[0].get('concepturi')
     return None
 
-# Reconcile IDs
-data['musicbrainz_url'] = data['id'].apply(lambda x: reconcile_musicbrainz(x))
+def get_id_type(column : str):
+    keywords = column.split("_")
+    word = keywords[-2]
+    word = re.sub(r'\d+', '', word)
+    return word
 
 # Reconcile names, titles, and genres using Wikidata
-for col in ['title', 'genres1_name', 'genres2_name', 'genres3_name']:
-    if col in data.columns:
-        data[f'{col}_wikidata'] = data[col].apply(lambda x: reconcile_wikidata(x) if pd.notna(x) else None)
+for col in data.columns:
+    print(col)
+    if col == 'title' :
+        OUTDATA[f'{col}_wk_url'] = data[col].apply(lambda x: reconcile_wikidata(x, 'single') if pd.notna(x) else None)
+    elif 'name' in col:
+        OUTDATA[f'{col}_wk_url'] = data[col].apply(lambda x: reconcile_wikidata(x) if pd.notna(x) else None)
+    elif col == 'id':
+        OUTDATA['absolute_url'] = data[col].apply(lambda x: reconcile_musicbrainz(x))
+    elif 'ids' in col:
+        keyword = get_id_type(col)
+        OUTDATA[f'{col}_musicbrainz_url'] = data[col].apply(lambda x: reconcile_musicbrainz(x, entity_type=keyword) if pd.notna(x) else None)
+        print(keyword, OUTDATA[f'{col}_musicbrainz_url'])
+    else:
+        OUTDATA[col] = data[col]
+        
+OUTDATA.to_csv(OUTNAME, index=False)
