874a28a779bda9fba625150120b074e573f5da4d
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-06-05 10:19:09 -0400

    Create get_relations.py

---
+++ cantusdb/turtle/get_relations.py
@@ -0,0 +1,23 @@
+import csv
+import json
+import sys
+
+filenames = sys.argv[1:]
+output_name = "test.json"
+dt = {}
+
+for filename in filenames:
+    with open(filename, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        header = next(csv_reader)
+        
+    header.append("type")
+
+    for item in header:
+        if item == "type":
+            dt[item] = []
+        else:
+            dt[item] = ""
+
+with open(output_name, "w") as out_json:
+    json.dump(dt, out_json, indent=4)
