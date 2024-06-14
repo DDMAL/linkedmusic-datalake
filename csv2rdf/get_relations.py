e9979d8743d7956894ec5e5de61c4d4945d61a9e
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-06-14 10:43:15 -0400

    style: use pylint and black to reformat

--- csv2rdf/get_relations.py
+++ csv2rdf/get_relations.py
@@ -6,23 +6,24 @@ For all the CSV files, this script extracts their headers and write them
 to a JSON file, which is used as the mapping file for the 
 csv2rdf_single_subject.py script.
 """
+
 import csv
 import json
 import sys
 import os
 
 filenames = sys.argv[1:]
-output_name = "mapping.json"
+OUTPUT_NAME = "mapping.json"
 dt = {}
 dt["entity_type"] = []
 
 for filename in filenames:
-    with open(os.path.abspath(filename), "r") as csv_file:
+    with open(os.path.abspath(filename), "r", encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
         header = next(csv_reader)
-        
+
     for item in header:
         dt[item] = ""
-        
-with open(output_name, "w") as out_json:
+
+with open(OUTPUT_NAME, "w", encoding='utf-8') as out_json:
     json.dump(dt, out_json, indent=4)
