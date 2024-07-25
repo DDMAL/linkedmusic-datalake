3cacb712dcd97e8a735cce86732a375fbfeb239f
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-07-25 09:59:38 -0400

    refactor: requests update

--- musicbrainz/csv/get_genre.py
+++ musicbrainz/csv/get_genre.py
@@ -4,6 +4,7 @@ Script for retrieving the "genre" dumps since they are not available online.
 
 import time
 import requests
+from requests.exceptions import HTTPError
 import pandas as pd
 from bs4 import BeautifulSoup
 
@@ -15,15 +16,26 @@ max_records = dict(requests.get(url=URL, params=PARAMS, timeout=500).json())[
     "genre-count"
 ]
 
-for i in range(0, max_records, 50):
+for i in range(0, max_records):
     PARAMS["offset"] = i
-    resp = requests.get(url=URL, params=PARAMS, timeout=500)
-    if resp.status_code == 503:
-        time.sleep(1)
-        resp = requests.get(url=URL, params=PARAMS, timeout=500)
+    for j in range(0, 3):
+        try:
+            resp = requests.get(url=URL, params=PARAMS, timeout=500)
+            resp.raise_for_status()
+            break
+
+        except HTTPError as exc:
+            code = exc.response.status_code
+
+            if code == 503:
+                time.sleep(j)
+                continue
+
+            raise
 
     temp = resp.json()
-    data += dict(temp)["genres"]
+    data += temp["genres"]
+    time.sleep(0.02)
 
 
 df = pd.DataFrame(data)
@@ -35,13 +47,16 @@ relations_wiki = []
 
 for rec in df["genre_id"]:
     resp_wiki = requests.get(rec, timeout=500)
-    resp_wiki.raise_for_status()
+
+
     soup = BeautifulSoup(resp_wiki.text, "html.parser")
     wikidata_row = soup.find("th", string="Wikidata:")
     wikidata_value = ""
     if wikidata_row:
         wikidata_value = wikidata_row.find_next_sibling("td").find("a").text
-    relations_wiki.append("http://www.wikidata.org/entity/" + wikidata_value)
+        relations_wiki.append("http://www.wikidata.org/entity/" + wikidata_value)
+    else:
+        relations_wiki.append()
 df["relations_wiki"] = relations_wiki
 
 df.to_csv("../data/genre.csv", index=False)
