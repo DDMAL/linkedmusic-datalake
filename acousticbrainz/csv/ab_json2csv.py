ac0999ed91ff14642ec9848cb9fee6e200015faf
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-07-31 10:08:18 -0400

    refactor: add back the columns.

--- acousticbrainz/csv/ab_json2csv.py
+++ acousticbrainz/csv/ab_json2csv.py
@@ -26,7 +26,7 @@ def expand_lists(dataframe, list_columns):
 for json_file in glob.glob(f"{JSON_FILES_PATH}/**/{PATTERN}", recursive=True):
     with open(json_file, encoding="utf-8") as data_file:
         data = json.load(data_file)
-        df = pd.json_normalize(data, max_level=2)
+        df = pd.json_normalize(data)
         df_list.append(df)
 
 # Convert to CSV
@@ -45,27 +45,12 @@ df_merged = df_merged[
 columns_to_drop = []
 columns_with_lists = []
 
-# The floats are the percentages for the genre features extraction.
-# Those data are too overwhelming and makes my inspection much more difficult.
-# Sometimes python sees an ID as a float, so I want to keep those.
-# If we normalize the higher level dictionaries, they makes the chart very large,
-# and if we not normalize the higher level dictionaries,
-# they will be left as dictionaries in the CSV, so not good as well.
-# Therefore I decide to drop any dictionaries after level 2.
 
 for column in df_merged.columns:
-    if (
-        df_merged[column].apply(lambda x: isinstance(x, dict)).any()
-        or df_merged[column].apply(lambda x: isinstance(x, float)).any()
-    ) and not column.endswith("id"):
-        # Mark column for dropping if any element is a dictionary or float
-        columns_to_drop.append(column)
-    elif df_merged[column].apply(lambda x: isinstance(x, list)).any():
+    if df_merged[column].apply(lambda x: isinstance(x, list)).any():
         # Convert lists to strings
         columns_with_lists.append(column)
 
-# Drop columns that contain dictionaries
-df_merged.drop(columns=columns_to_drop, inplace=True)
 df_merged = expand_lists(df_merged, columns_with_lists)
 for column in df_merged.columns:
     if column.endswith("id"):
