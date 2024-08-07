c4d8dc625180a368adf3b9396731636eef008698
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-08-07 13:09:47 -0400

    feat: extract thesession artist urls

---
+++ thesession/flattening/data/find_artist.py
@@ -0,0 +1,25 @@
+import requests
+import pandas
+from bs4 import BeautifulSoup
+
+
+def get_artist_url(page_url, artist_name):
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Find the link with the specific artist name
+    artist_section = soup.find(string=lambda text: text and artist_name in text)
+    if artist_section:
+        artist_link = artist_section.find_next("a", href=True)
+        if artist_link:
+            return "https://thesession.org/" + artist_link["href"]
+    return None
+
+
+df = pandas.read_csv("reconciled/recordings.csv")
+
+artist_name = "By "
+
+# Get the URL of the artist
+df["artist_url"] = df["recording_id"].apply(lambda x: get_artist_url(x, artist_name))
+df.to_csv("reconciled/recordings.csv", index=False)
