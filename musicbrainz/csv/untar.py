9d5724b221611294a5086a82bc04898a95210079
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-08-08 11:03:12 -0400

    refactor: extract only mbdump

--- musicbrainz/csv/untar.py
+++ musicbrainz/csv/untar.py
@@ -5,13 +5,14 @@ def extract_file(folderpath, dest_folder):
     """
     untar the downloaded files
     """
-    for filepath in glob.glob(folderpath, recursive=False):
-        if filepath.endswith(".tar.xz"):
-            with tarfile.open(f"{filepath}/mbdump/", "r:xz") as tar:
-                tar.extractall(path=dest_folder)
-            print(f"Extracted {filepath} to {dest_folder}")
+    for filepath in glob.glob(f"{folderpath}/*.csv", recursive=False):
+        with tarfile.open(f"{filepath}", "r:xz") as tar:
+            for member in tar.getmembers():
+                if member.name.startswith("mbdump"):
+                    tar.extractall(path=dest_folder)
+        print(f"Extracted {filepath} to {dest_folder}")
     
 
 DEST_FOLDER = "../data/raw"
         
-extract_file(DEST_FOLDER, DEST_FOLDER + "/jsonl")
+extract_file(DEST_FOLDER, DEST_FOLDER + "/extracted_jsonl")
