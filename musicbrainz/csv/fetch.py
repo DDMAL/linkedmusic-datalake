0e1338d79c79ca935d09e3081a010d41ed977156
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2024-08-19 10:13:42 -0400

    refactor: optimize file list

--- musicbrainz/csv/fetch.py
+++ musicbrainz/csv/fetch.py
@@ -5,8 +5,6 @@ fetch the latest test data dump files
 import os
 import time
 import requests
-from bs4 import BeautifulSoup
-
 
 def get_latest_json_dump_url():
     """
@@ -14,32 +12,37 @@ def get_latest_json_dump_url():
     """
     url = "https://data.metabrainz.org/pub/musicbrainz/data/json-dumps/LATEST"
     resp = requests.get(url, timeout=50).text.strip()
-    return resp
+    return "https://data.metabrainz.org/pub/musicbrainz/data/json-dumps/" + resp
 
 
 def fetch_api_call(url):
     """
     API calls to fetch all .tar.xz files
     """
-    response = requests.get(url, timeout=50)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    # Find all links ending with .tar.xz
     tar_xz_files = [
-        url + a["href"] for a in soup.find_all("a") if a["href"].endswith(".tar.xz")
+        "area.tar.xz",
+        "artist.tar.xz",
+        "event.tar.xz",
+        "instrument.tar.xz",
+        "label.tar.xz",
+        "place.tar.xz",
+        "recording.tar.xz",
+        "release-group.tar.xz",
+        "release.tar.xz",
+        "series.tar.xz",
+        "work.tar.xz"
     ]
 
-    for src_url in tar_xz_files:
-        response = requests.get(src_url, timeout=50)
-
-        local_filename = os.path.join(RAW_PATH, src_url.split("/")[-1])
-        with requests.get(src_url, stream=True, timeout=50) as r:
+    for file in tar_xz_files:
+        file_path = os.path.join(url, file)
+        local_path = os.path.join(RAW_PATH, file)
+        with requests.get(file_path, stream=True, timeout=50) as r:
             r.raise_for_status()
-            with open(local_filename, "wb") as f:
+            with open(local_path, "wb") as f:
                 for chunk in r.iter_content(chunk_size=8192):
                     f.write(chunk)
 
-        print(f"downloaded {local_filename}")
+        print(f"downloaded {local_path}")
         time.sleep(1)
 
 
