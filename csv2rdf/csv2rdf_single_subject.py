1a791bc84013170515331d3aef0dadb9bd27ad30
Junjun Cao <63447377+candlecao@users.noreply.github.com>
2024-09-09 14:56:38 -0400

    Update csv2rdf_single_subject.py

--- csv2rdf/csv2rdf_single_subject.py
+++ csv2rdf/csv2rdf_single_subject.py
@@ -118,7 +118,7 @@ def convert_csv_to_turtle(filenames: List[str]) -> Graph:
 
                             day_of_week = datetime_obj.strftime("%A")
                             day_of_week_obj = Literal(day_of_week)
-                            g.add((key_attribute, predicates[i], day_of_week_obj))
+                            g.add((key_attribute, URIRef("http://www.wikidata.org/prop/direct/P2894"), day_of_week_obj))
 
                             day_str = datetime_obj.strftime("%Y-%m-%dT%H:%M:%S")
                             obj = Literal(day_str, datatype=XSD.dateTime)
