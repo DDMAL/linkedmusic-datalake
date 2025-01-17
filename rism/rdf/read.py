1f38beea21edb8a8267d64d9b0b94ae5d7b3a72c
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2025-01-17 12:58:16 -0500

    refactor: only reads and output the prettier version

--- rism/rdf/read.py
+++ rism/rdf/read.py
@@ -1,32 +1,21 @@
 from rdflib import Graph
-import validators
-import pandas as pd
 
-FILE_PATH = "rismTitleRDFexample.ttl"  # Replace with your RDF file
+FILE_PATH = "rism-test-100000.ttl"  # Replace with your RDF file
+
 
 def load_rdf(file_path: str, f):
     """
     load RDF data from a file
     """
     # Create a new RDF graph
-    g = Graph()
+    graph = Graph()
     # Parse the RDF data from the specified file
-    g.parse(file_path, format=f)
+    graph.parse(file_path, format=f)
 
-    rows = {}
-    # Print all triples in the graph
-    for subj, pred, obj in g:
-        if not validators.url(obj):
-            print(f"Subject: {subj}, Predicate: {pred}, Object: {obj}")
-            try:
-                rows[subj][pred] = obj
-            except KeyError:
-                rows[subj] = {pred: obj}
-
-    df = pd.DataFrame.from_dict(rows, orient='index')
-    df.to_csv("output.csv")
+    return graph
 
 
 # Example usage
 if __name__ == "__main__":
-    load_rdf(FILE_PATH, "ttl")
+    g = load_rdf(FILE_PATH, "ttl")
+    g.serialize("output.ttl", format="ttl")
