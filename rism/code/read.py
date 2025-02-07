411b57262eb85b3cb6e1a4b0eae1c4fa470d1c62
Yueqiao Zhang <yueqiao.zhang@mail.mcgill.ca>
2025-02-07 11:28:34 -0500

    feat: separate graph feature completed

--- rism/code/read.py
+++ rism/code/read.py
@@ -1,21 +1,53 @@
-from rdflib import Graph
+from rdflib import Graph, BNode, URIRef
 
 FILE_PATH = "../data/raw/rism-test-100000.ttl"  # Replace with your RDF file
 
 
-def load_rdf(file_path: str, f):
+def parse_bnode(graph: Graph, bnode: BNode):
     """
-    load RDF data from a file
+    Parse a BNode from an RDF graph
+    """
+    subgraph = Graph()
+    for triples in graph.triples((bnode, None, None)):
+        subgraph.add(triples)
+        if isinstance(triples[2], BNode):
+            subgraph += parse_bnode(graph, triples[2])
+    return subgraph
+
+
+def load_rdf(file_path: str, f, chunk_size=1e6):
+    """
+    Load RDF data from a file and split into chunks
+    Returns: List of Graph objects, each containing chunk_size triples
     """
     # Create a new RDF graph
     graph = Graph()
     # Parse the RDF data from the specified file
     graph.parse(file_path, format=f)
+    chunks = []
+    subgraph = Graph()
+    subjects = set([s for s in graph.subjects() if isinstance(s, URIRef)])
 
-    return graph
+    for s in subjects:
+        for triples in graph.triples((s, None, None)):
+            subgraph.add(triples)
+
+            if isinstance(triples[2], BNode):
+                subgraph += parse_bnode(graph, triples[2])
+
+            if len(subgraph) >= chunk_size:
+                chunks.append(subgraph)
+                subgraph = Graph()
+
+    chunks.append(subgraph)
+
+    return chunks
 
 
 # Example usage
 if __name__ == "__main__":
-    g = load_rdf(FILE_PATH, "ttl")
-    g.serialize("../data/raw/output.ttl", format="ttl")
+    graphs = load_rdf(FILE_PATH, "ttl")
+    counter = 0
+    for g in graphs:
+        g.serialize(f"../data/raw/output{counter}.ttl", format="ttl")
+        counter += 1
